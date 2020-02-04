92701fdfe72278348e210ac2b6874bc52dadcb43
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-04 12:59:09 +0530

    created server for create post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app =express();
 const signUp = require('../routes/signup_login');
 app.use("/user",signUp);
 
+creat_post = require("../routes/create_post");
+app.use("/post",creat_post)
+
 
 
 app.listen(5000,()=>{
