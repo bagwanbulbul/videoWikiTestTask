a5f10b80c294ba29520fce9af5e928a31262a4a0
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-06 16:33:08 +0530

    query for show user name

--- model/edit_profile_db.js
+++ model/edit_profile_db.js
@@ -2,6 +2,9 @@ const knex = require("../conection.js")
 //  profile = ()=>{
 //      return knex.select("name").from("regisetr")
 //  }
+let showUserName =(user_id)=>{
+    return knex.select("name").from("user_register").where("user_id",user_id)
+}
 
 let profile=(user_id)=>{
     return knex.select('*').from('user_register').havingIn('user_id',user_id)
@@ -11,4 +14,4 @@ let profile=(user_id)=>{
 let myProfile = (user_data)=>{
     return knex.select("*").from("edit_profile").insert(user_data)
 }
-module.exports={profile,myProfile}
+module.exports={profile,myProfile,showUserName}
