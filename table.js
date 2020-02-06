8e1f3195056ec588bc2c918165f75ecdf1e42a9c
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-06 15:06:29 +0530

    created table for edit profile

--- table.js
+++ table.js
@@ -7,11 +7,17 @@ var knex = require('knex')({
       database: 'videoWikiTest'
     },
   })
-knex.schema.createTable('create_post', (table) => {
-    table.increments('post_id'),
-    table.integer("user_id"),
-    table.string("post"),
-    table.string("caption")
+knex.schema.createTable('edit_profile', (table) => {
+    // table.integer('post_id'),
+    // table.integer("user_id"),
+    // table.boolean("likes"),
+    // table.string("comments")
+    table.integer("phone_number"),
+    table.string("email_id"),
+    table.string("birth_date"),
+    table.string("education"),
+    table.string("address"),
+    table.string("bio")
 })
 .then(() => console.log("table created"))
 .catch((err) => { console.log(err); throw err });
