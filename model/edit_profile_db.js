fdf527ab0970db0cf7d8b319ab02deda8126ab54
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-06 15:04:04 +0530

    query for edit profile

---
+++ model/edit_profile_db.js
@@ -0,0 +1,14 @@
+const knex = require("../conection.js")
+//  profile = ()=>{
+//      return knex.select("name").from("regisetr")
+//  }
+
+let profile=(user_id)=>{
+    return knex.select('*').from('user_register').havingIn('user_id',user_id)
+    // .from("edit_profile").insert(user_data)
+
+}
+let myProfile = (user_data)=>{
+    return knex.select("*").from("edit_profile").insert(user_data)
+}
+module.exports={profile,myProfile}
