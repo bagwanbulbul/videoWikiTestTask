d52016bcd5174718d6851e83c9ebc4412eeb89d1
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-06 15:03:09 +0530

    created route for edit profile

--- server/index.js
+++ server/index.js
@@ -6,9 +6,15 @@ app.use("/user",signUp);
 creat_post = require("../routes/create_post");
 app.use("/post",creat_post)
 
+user_likes = require("../routes/user_likes");
+app.use("/likes",user_likes)
+
+editProfile = require("../routes/edit_profile");
+app.use("/profile",editProfile)
+
 
 
 app.listen(5000,()=>{
-    console.log("server is listening 5500 ....")
+    console.log("server is listening 5000 ....")
 
 });
