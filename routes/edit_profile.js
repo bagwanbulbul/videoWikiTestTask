6db3cfd56724fd2e0b626032bcfced5e97e09782
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-02-06 15:05:11 +0530

    created route for edit profile

---
+++ routes/edit_profile.js
@@ -0,0 +1,41 @@
+const express = require('express');
+var editProfile = express.Router();
+editProfile.use(express.json())
+const add = require("../model/edit_profile_db");
+const jwt = require('jsonwebtoken')
+
+
+editProfile.post("/profile",function(req,res){
+    let user_id = req.body.user_id
+    let user_data = {
+        phone_number:req.body.phone_number,
+        email_id:req.body.email_id,
+        birth_date:req.body.birth_date,
+        education:req.body.education,
+        address:req.body.address,
+        bio:req.body.bio
+
+    }
+    let response = add.profile(user_id,user_data)
+    response.then((data)=>{
+        if(data[0]["user_id"]==user_id){
+            let token = jwt.sign({"user":data[0]},"secret_key")
+            res.cookie(token)
+            jwt.verify(token,"secret_key",(err,result)=>{
+                add.myProfile(user_data)
+                .then((data)=>{
+                    res.json({"massage":"profile updated","data":data})
+                })
+                // res.json({"status":"write","massage":"login successful ","token":result})
+            }) 
+        }else{
+            console.log("errrrr")
+        }
+    }).catch((err)=>{
+        console.log(err)
+        res.send(err)
+    })
+    
+})
+
+module.exports = editProfile
